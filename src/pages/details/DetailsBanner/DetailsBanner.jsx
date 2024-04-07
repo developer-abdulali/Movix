aeff2567e8e3864dcd960d2422ca8ece4ef68fcc
developer-abdulali <ab.ali.soomro@gmail.com>
2024-04-08 03:33:18 +0500

    added changes

--- src/pages/details/DetailsBanner/DetailsBanner.jsx
+++ src/pages/details/DetailsBanner/DetailsBanner.jsx
@@ -9,12 +9,17 @@ import Genres from "../../../components/Genres/Genres.jsx";
 import CircleRating from "../../../components//CircleRating/CircleRating.jsx";
 import Img from "../../../components/LazyLoadImg/img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
+import { PlayIcon } from "../PlayBtn.jsx";
 
 const DetailsBanner = ({ video, crew }) => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
   const { url } = useSelector((state) => state.home);
-
+  const _genres = data?.genres?.map((g) => g.id);
+  const director = crew?.filter((f) => f.job === "Director");
+  const writer = crew?.filter(
+    (f) => f.job === "Screenplay ||" || f.job === "Story" || f.job === "Writer"
+  );
   const toHoursAndMinutes = (totalMinutes) => {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
@@ -28,7 +33,7 @@ const DetailsBanner = ({ video, crew }) => {
           {!!data && (
             <React.Fragment>
               <div className="backdrop-img">
-                <Img src={url.backdrop + data?.backdrop_path} />
+                <Img src={url?.backdrop + data?.backdrop_path} />
               </div>
               <div className="opacity-layer" />
               <ContentWrapper>
@@ -50,6 +55,55 @@ const DetailsBanner = ({ video, crew }) => {
                       ).format("YYYY")})`}
                     </div>
                     <div className="subtitle">{data.tagline}</div>
+                    <Genres data={_genres} />
+                    <div className="row">
+                      <CircleRating rating={data.vote_average.toFixed(1)} />
+                      <div className="playbtn" onClick={() => {}}>
+                        <PlayIcon />
+                        <span className="text">Watch Trailer</span>
+                      </div>
+                    </div>
+                    <div className="overview">
+                      <div className="heading">overview</div>
+                      <div className="description">{data.overview}</div>
+                    </div>
+                    <div className="info">
+                      {data.status && (
+                        <div className="infoItem">
+                          <span className="text bold">Status: </span>
+                          <span className="text">{data.status}</span>
+                        </div>
+                      )}
+                      {data.release_date && (
+                        <div className="infoItem">
+                          <span className="text bold">Release Date: </span>
+                          <span className="text">
+                            {dayjs(data.release_date).format("MMM D, YYYY")}
+                          </span>
+                        </div>
+                      )}
+                      {data.runtime && (
+                        <div className="infoItem">
+                          <span className="text bold">Run Time: </span>
+                          <span className="text">
+                            {toHoursAndMinutes(data.runtime)}
+                          </span>
+                        </div>
+                      )}
+                    </div>
+                    {director?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Director: </span>
+                        <span className="text">
+                          {director?.map((d, i) => (
+                            <span key={i}>
+                              {d?.name}
+                              {director.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
                   </div>
                 </div>
               </ContentWrapper>

2fbe31da8c27a77e468eae8858149db4cb5dfb31
developer-abdulali <ab.ali.soomro@gmail.com>
2024-04-09 00:40:53 +0500

    Update HeroBanner.jsx

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -38,7 +38,7 @@ const HeroBanner = () => {
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
           <span className="subTitle">
-            Millions of movies, TV showa and people to discover. Explore now.
+            Millions of movies, TV shows and people to discover. Explore now.
           </span>
           <div className="searchInput">
             <input
